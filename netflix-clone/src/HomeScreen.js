2b802c24ab6b403ab26f3f4332f7e805a6293c1d
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-04 09:37:01 +0300

    fetched the movies images for the roes of movies

--- netflix-clone/src/HomeScreen.js
+++ netflix-clone/src/HomeScreen.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./HomeScreen.css"
 import Nav from "./Nav.js"
 import Banner from "./Banner.js"
+import requests from "./Request";
+import Row from "./Row";
 
 function HomeScreen() {
     return <div className="HomeScreen">
         <Nav />
+
         <Banner />
 
-        {/* Row */}
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
 }
 
