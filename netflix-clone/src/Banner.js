f55dc2282b217c5a61fcded326be0b76f54294a1
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-04 08:43:24 +0300

    Applied Netflix Originals movie API on the banner

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import "./Banner.css"
-import netflixBanner from "./images/netflix-banner.webp"
+import React, { useEffect, useState } from 'react';
+import "./Banner.css";
+import netflixBanner from "./images/netflix-banner.webp";
+import axios from "./axios";
+import requests from './Request';
 
 function Banner() {
 
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        } 
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
+    //creating the truncate long paragraphs function
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
     }
@@ -11,19 +31,20 @@ function Banner() {
   return (
     <header className='banner'
     style={{backgroundSize: "cover",
-            backgroundImage: `url(${netflixBanner})`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
     }}
     >
         <div className='banner-contents'>
-            <h1 className='banner-title'>Movie Name</h1>
+            <h1 className='banner-title'>
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
             <div className='banner-buttons'>
                 <button className='banner-button'>Play</button>
                 <button className='banner-button'>My List</button>
             </div>
             <h1 className='banner-description'>
-                {truncate(`The coolest movie in this session The coolest movie in this session
-                The coolest movie in this session The coolest movie in this session`,150)}    
+                {truncate(movie?.overview,150)}    
             </h1>
         </div>
 
