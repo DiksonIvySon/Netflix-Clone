d614cb3b5ac1a47cc6e28ba780a9b3a2f0c63f28
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-09 17:29:46 +0300

    Installed and tested React Router

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import HomeScreen from './HomeScreen.js'
-
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
