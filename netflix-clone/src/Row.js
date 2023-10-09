109b0af2a17fa390cb531bbcb5483ec1f5108478
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-09 17:03:52 +0300

    Deleted all uncommited changes and made new changes

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -24,7 +24,8 @@ function Row({title, fetchUrl, isLargeRow = false}) {
     <div className='row'>
         <h2>{title}</h2>
         <div className='row_posters'>
-            {movies.map((movie) => (
+            {movies.map((movie) => 
+                ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
                 <img
                     className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                     key={movie.id} 
