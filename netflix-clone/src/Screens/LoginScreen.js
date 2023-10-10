3d6317b4e2a5027310747e57f175e15a49c286f9
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-10 08:38:03 +0300

    Created a use state to change when the buttens are clicked

--- netflix-clone/src/Screens/LoginScreen.js
+++ netflix-clone/src/Screens/LoginScreen.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./LoginScreen.css";
 import netflixLogo from "../images/netflixLogo-transparent.png";
 
 function LoginScreen() {
+
+  const [SignIn, setSignIn] = useState(false);
+
   return (
     <div className='loginScreen'>
         <div className='loginScreen-background'>
             <img className='loginScreen-logo' src={netflixLogo} alt=''/>
-            <button className='loginScreen-button'>Sign In</button>
+            <button className='loginScreen-button' onClick={() => setSignIn(true)}>Sign In</button>
             <div className='loginScreen-gradient'/>
         </div>
         <div className='loginScreen-body'>
-            <h1>Unlimited films, TV programmes and more.</h1>
-            <h2>Watch anywhere. Cancel at any time.</h2>
-            <h3>Ready to watch? Enter your Email to create or restart your membership.</h3>
+            {SignIn ? (
+                <SignInScreen />
+            ) : (
+                <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at any time.</h2>
+                    <h3>Ready to watch? Enter your Email to create or restart your membership.</h3>
 
-            <div className='loginScreen-input'>
-                <form>
-                    <input type='email' placeholder='Email' name='email' />
-                    <button className='loginScreen-getStarted'>GET STARTED</button>
-                </form>
-            </div>
+                    <div className='loginScreen-input'>
+                        <form>
+                            <input type='email' placeholder='Email' name='email' />
+                            <button className='loginScreen-getStarted' onClick={() => setSignIn(true)}>GET STARTED</button>
+                        </form>
+                    </div>
+                </>
+            )}
         </div>
     </div>
   )
