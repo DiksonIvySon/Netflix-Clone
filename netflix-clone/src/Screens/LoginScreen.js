2f990de335e13a7848a70af98b5a2182f93a64b1
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-10 08:12:18 +0300

    Created the login screen component and styled it

--- netflix-clone/src/Screens/LoginScreen.js
+++ netflix-clone/src/Screens/LoginScreen.js
@@ -10,6 +10,18 @@ function LoginScreen() {
             <button className='loginScreen-button'>Sign In</button>
             <div className='loginScreen-gradient'/>
         </div>
+        <div className='loginScreen-body'>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>Ready to watch? Enter your Email to create or restart your membership.</h3>
+
+            <div className='loginScreen-input'>
+                <form>
+                    <input type='email' placeholder='Email' name='email' />
+                    <button className='loginScreen-getStarted'>GET STARTED</button>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
